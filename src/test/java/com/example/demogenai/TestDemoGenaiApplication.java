02a3f7ec031b8a89c065c14f391932f5bf090c01
Ignasi <ignasi.lopez.luna@gmail.com>
2024-05-06 11:37:50 +0200

    Fix TestDemoGenaiApplication for local development

--- src/test/java/com/example/demogenai/TestDemoGenaiApplication.java
+++ src/test/java/com/example/demogenai/TestDemoGenaiApplication.java
@@ -1,12 +1,12 @@
 package com.example.demogenai;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.test.context.TestConfiguration;
 
-@TestConfiguration(proxyBeanMethods = false)
 public class TestDemoGenaiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(DemoGenaiApplication::main).with(TestDemoGenaiApplication.class).run(args);
+        SpringApplication.from(DemoGenaiApplication::main)
+                .with(ContainersConfiguration.class, IngestionConfiguration.class)
+                .run(args);
     }
 }
